1430ae8a08b0d63cfc83579d6d96e5097fad28f3
robettes-programming <programming@therobettes.com>
2019-01-12 14:24:55 -0600

    Drive Train and Two Motors (Working)

---
+++ src/main/java/frc/robot/subsystems/DriveChassis.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class DriveChassis extends Subsystem {
+  private DifferentialDrive driving = new DifferentialDrive(RobotMap.leftDrive, RobotMap.rightDrive);
+
+ public void arcadeDrive(double speed, double rotation ){
+     driving.arcadeDrive(speed, rotation); 
+}
+
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
